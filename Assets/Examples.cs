848ce90e188bd68d33fe9189a2bcc6a360049195
Alexander Malyutin <alex.malyutin.dev@gmail.com>
2021-09-12 16:23:14 +0300

    Initial commit.

---
+++ Assets/Examples.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Core.Systems.Events;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+	public class EventA { }
+
+	public class Example1
+	{
+		public class Bar
+		{
+			public Bar(IEventBus eventBus)
+			{
+				eventBus.Fire(new EventA());
+			}
+		}
+
+		public class Foo : IDisposable
+		{
+			private IDisposable _sub;
+
+			public Foo(IEventBus eventBus)
+			{
+				_sub = eventBus.Subscribe<EventA>(e => Debug.Log(e));
+			}
+
+			public void Dispose()
+			{
+				_sub.Dispose();
+			}
+		}
+	}
+
+	public class Example2
+	{
+		public class Foo : IEventBusSubscriber, IDisposable
+		{
+			private readonly IEventBus _eventBus;
+
+			public Foo(IEventBus eventBus)
+			{
+				_eventBus = eventBus;
+				_eventBus.Subscribe(this);
+			}
+
+			public void HandleEvent<T>(T e)
+			{
+				switch (e)
+				{
+					case EventA a:
+						//...
+					break;
+				}
+			}
+
+			public void Dispose()
+			{
+				_eventBus.Unsubscribe(this);
+			}
+		}
+	}
+}
